f183d206e595623dc2afe7981ec4a560cd176b5d
promtkup <kantaphong.pol@gmail.com>
2023-04-28 19:38:40 +0700

    CharacterStream

---
+++ src/main/java/LAB3.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class LAB3 {
+    public static void main(String[] args) {
+        try {
+            PrintWriter output = new PrintWriter("data.txt");
+            output.println("Harry Potter");
+            output.println(19);
+            output.println(60.5);
+            output.println(175.0);
+            output.close();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(LAB3.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        try {
+            String s;
+            FileReader file = new FileReader("data.txt");
+            BufferedReader input = new BufferedReader(file);
+            while ((s = input.readLine()) != null) {
+                System.out.println(s);
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(LAB2.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(LAB2.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
